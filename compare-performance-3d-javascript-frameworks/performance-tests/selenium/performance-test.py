1ed84b8273319ead1b2491c14c4580753860c931
Julia <juliamjohansson@gmail.com>
2020-12-01 22:22:04 +0100

    Introduces working fps-counter, refactors file a bit

--- compare-performance-3d-javascript-frameworks/performance-tests/selenium/performance-test.py
+++ compare-performance-3d-javascript-frameworks/performance-tests/selenium/performance-test.py
@@ -1,15 +1,16 @@
 """
 To access and record performance data
 using Chrome webdrivers in headless mode
-fixa döpa om namn till python...
+run with 'python3 performance-test.py'
 """
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 def javascript():
-    with open('performance-test.js') as f:
+    with open('fps-counter.js') as f:
         content = f.readlines()
     js = "".join(content)
     javascript = "\
@@ -20,31 +21,35 @@ def javascript():
             .replace('\t','').replace("\n", "").replace('"','\\"'))
     return javascript
 
-
 chrome_options = Options()
-# chrome_options.headless = True
-# chrome_options.add_argument('--start-maximized')
+chrome_options.headless = True
+chrome_options.add_argument('--start-maximized')
 
 hyperlink = "https://modest-curran-4f504d.netlify.app/"
 
 browser = webdriver.Chrome(options=chrome_options)
 browser.get(hyperlink)
 
-
-navigationStart = browser.execute_script("return window.performance.timing.navigationStart")
-responseStart = browser.execute_script("return window.performance.timing.responseStart")
-domComplete = browser.execute_script("return window.performance.timing.domComplete")
+navigation_start = browser.execute_script("return window.performance.timing.navigationStart")
+response_start = browser.execute_script("return window.performance.timing.responseStart")
+dom_complete = browser.execute_script("return window.performance.timing.domComplete")
 memory = browser.execute_script("return window.performance.memory.totalJSHeapSize")
 
 browser.execute_script(javascript())
-# time.sleep(5)
+waiting = WebDriverWait(browser, 10).until(lambda driver: browser.execute_script("return window.fps_arr[5];"))
 fps_arr = browser.execute_script("return window.fps_arr")
 
-print("navigationStart: %s" % navigationStart) # timestamp
-print("responseStart: %s" % responseStart) # timestamp
-print("domComplete: %s" % domComplete) # timestamp
-print("TotalJSHeapSize: %s" % memory) # Kolla upp exakt mått
-print(fps_arr)
-# print(fps_arr)
+backend_performance_calc = response_start - navigation_start
+frontend_performance_calc = dom_complete - response_start
 
-# browser.quit()
+# print("navigation_start: %s" % navigation_start) # timestamp
+# print("response_start: %s" % response_start) # timestamp
+# print("dom_complete: %s" % dom_complete) # timestamp
+print("Back End: %s" % backend_performance_calc)
+print("Front End: %s" % frontend_performance_calc)
+print("Total js heap size: %s" % memory) # Kolla upp exakt mått
+# print(waiting)
+print(fps_arr)
+
+
+browser.quit()
